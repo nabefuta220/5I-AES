0be4e1a9f420bbeab668f6d346194c5872c21de3
nabefuta <satoshinabe@220@gmail.com>
2022-05-15 13:32:19 +0900

    mkmbox03を作成

---
+++ src/mkmbox03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "aes128.h"
+
+int main() {
+	printf("unsigned char mbox02[256]={");
+	for (int a = 0x00; a <= 0xff; a++) {
+		printf("0x%02x", Multiply(a, 0x03));
+		if (a != 0xff) printf(",");
+	}
+	printf("}\n");
+}
