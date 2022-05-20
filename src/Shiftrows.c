f68147aacf3b05669ddd7d1751db41602eb1dfa6
nabefuta <satoshinabe@220@gmail.com>
2022-05-21 01:35:38 +0900

    shiftrowsを修正

--- src/Shiftrows.c
+++ src/Shiftrows.c
@@ -4,7 +4,7 @@ void ShiftRows(unsigned char state[4 * Nb]) {
 	unsigned char temp[4 * Nb];
 	for (int i = 0; i < Nb; i++) {
 		for (int j = 0; j < 4; j++) {
-			temp[i + j * 4] = state[(4 * j + i) % (4 * Nb)];
+			temp[i + j * 4] = state[(4 * (j+i) + i) % (4 * Nb)];
 		}
 	}
 	for (int i = 0; i < 4 * Nb; i++) {
