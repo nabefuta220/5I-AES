21b65499507ce220a317871e8045db225f78b679
nabefuta <satoshinabe@220@gmail.com>
2022-05-21 01:44:19 +0900

    mixcolumnsを実装

---
+++ src/Mmixcolumns.c
@@ -0,0 +1,18 @@
+#include "aes128.h"
+
+void MixColumns(unsigned char state[4 * Nb]) {
+	unsigned char res[4 * Nb];
+	for (int i = 0; i < 4 * Nb; i += 4) {
+		res[i] = Multiply(0x02, state[i]) ^ Multiply(0x03, state[i + 1]) ^
+		         state[i + 2] ^ state[i + 3];
+		res[i + 1] = state[i] ^ Multiply(0x02, state[i + 1]) ^
+		             Multiply(0x03, state[i + 2]) ^ state[i + 3];
+		res[i + 2] = state[i] ^ state[i + 1] ^ Multiply(state[i + 2], 0x02) ^
+		             Multiply(state[i + 3], 0x03);
+		res[i + 3] = Multiply(0x03, state[i]) ^ state[i + 1] ^ state[i + 2] ^
+		             Multiply(0x02, state[i + 3]);
+	}
+	for (int i = 0; i < 4 * Nb; i++) {
+		state[i] = res[i];
+	}
+}
