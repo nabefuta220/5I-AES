5b73277932ab4b3c3f5bf18234a8b2000b5f9f13
nabefuta <satoshinabe@220@gmail.com>
2022-05-21 20:28:04 +0900

    繰り返し2乗法を用いたinverseを実装

---
+++ src/finverse.c
@@ -0,0 +1,37 @@
+/**
+ * @file Finverse.c
+ * @author Watanabe Satoshi
+ * @brief inverseを繰り返し二乗法を用いて高速化したプログラム
+ * @version 0.1
+ * @date 2022-05-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "aes128.h"
+
+/**
+ * @brief a^n(mod x^8 + x^4 + x^3 + x + 1)を求める
+ *
+ * @param a
+ * @param n
+ * @return unsigned char a^n(mod x^8 + x^4 + x^3 + x + 1)
+ */
+unsigned char pow(unsigned char a, int n);
+
+unsigned char Inverse(unsigned char b) { return pow(b, 254); }
+
+unsigned char pow(unsigned char a, int n) {
+	unsigned char res = 1;
+
+	if (n == 0) {
+		return res;
+	}
+	res = pow(a, n / 2);
+	res = Multiply(res, res);
+	if (n & 1) {
+		res = Multiply(res, a);
+	}
+	return res;
+}
