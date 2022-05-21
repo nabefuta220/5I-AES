8961a483f5f473362bfd092d36c29b8a88dd10ef
nabefuta <satoshinabe@220@gmail.com>
2022-05-21 19:01:51 +0900

    Finverseを作成

---
+++ src/Finverse.c
@@ -0,0 +1,27 @@
+/**
+ * @file Finverse.c
+ * @author Watanabe Satoshi
+ * @brief inverseをアフィン変換を用いて高速化したプログラム
+ * @version 0.1
+ * @date 2022-05-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "aes128.h"
+#include "ibox.c"
+
+unsigned char Inverse(unsigned char b) {
+	unsigned char res = 0;
+
+	for (int i = 0; i < 8; i++) {
+        if( b & (1<<i)){
+			res ^= ibox[i];
+		}
+	}
+    if(res & (1<<7)){
+		res ^= (0b100011011);
+	}
+	return res;
+}
