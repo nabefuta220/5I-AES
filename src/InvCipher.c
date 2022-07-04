4d25e930981018f3dc238f84826649fc37d18b6d
nabefuta <satoshinabe@220@gmail.com>
2022-07-04 23:29:06 +0900

    空白を修正

--- src/InvCipher.c
+++ src/InvCipher.c
@@ -11,31 +11,31 @@ void InvCipher(unsigned char in[4 * Nb], unsigned char out[4 * Nb],
 	for (int i = 0; i < 4 * Nb; i++) {
 		out[i] = in[i];
 	}
-	printf("round[%2d].iinput  ", 0);
+	printf("round[%2d].iinput ", 0);
 	PrintBlock(out);
 
 	AddRoundKey(out, w);
-	printf("round[%2d].ik_sch  ", 0);
+	printf("round[%2d].ik_sch ", 0);
 	PrintKey(w);
 
 	for (int round = 0; round < Nr; round++) {
-		printf("round[%2d].istart  ", round + 1);
+		printf("round[%2d].istart ", round + 1);
 		PrintBlock(out);
 
 		InvShiftRows(out);
-		printf("round[%2d].is_row  ", round + 1);
+		printf("round[%2d].is_row ", round + 1);
 		PrintBlock(out);
 
 		InvSubBytes(out);
-		printf("round[%2d].is_box  ", round + 1);
+		printf("round[%2d].is_box ", round + 1);
 		PrintBlock(out);
 
 		AddRoundKey(out, (long unsigned int *)(w + Nb * (round + 1)));
 
-		printf("round[%2d].ik_sch  ", round + 1);
+		printf("round[%2d].ik_sch ", round + 1);
 		PrintKey((long unsigned int *)(w + Nb * (round + 1)));
 		if (round != 10) {
-			printf("round[%2d].ik_add  ", round + 1);
+			printf("round[%2d].ik_add ", round + 1);
 			PrintBlock(out);
 
 			InvMixColumns(out);
