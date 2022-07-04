7f1a07328f80ad7f6a3f05203d5024c06b0e00c5
nabefuta <satoshinabe@220@gmail.com>
2022-07-04 20:06:42 +0900

    コードを修正

--- src/Invshiftrows.c
+++ src/Invshiftrows.c
@@ -4,7 +4,7 @@ void InvShiftRows(unsigned char state[4 * Nb]) {
 	unsigned char res[4 * Nb];
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < Nb; j++) {
-			res[(4 * (j + i) + i) % (4 * Nb)] = state[i + j * 4];
+			res[( (j + i)%Nb *4 + i) % (4 * Nb)] = state[i + j * 4];
 		}
 	}
 
